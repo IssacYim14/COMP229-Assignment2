235b737983ecbfd322dda0487aa2663d27b1bf91
IssacYim14 <134939017+IssacYim14@users.noreply.github.com>
2023-06-03 11:59:49 -0400

    added routes

--- server.js
+++ server.js
@@ -1,37 +1,21 @@
 const express = require('express');
-//let ejs = require('ejs');
-const path = require('path');
 const app = express();
 
-// setup static and middleware
 app.use(express.static('./public'))
 //app.set('views', './view');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('home', {title : 'Home Page'});
-    //res.sendFile(path.resolve(__dirname, './home.html'))
-})
+const homeRoute = require('./routes/home');
+const aboutRoute = require('./routes/about');
+const projectsRoute = require('./routes/projects');
+const servicesRoute = require('./routes/services');
+const contactRoute = require('./routes/contact');
 
-app.get('/about', (req, res) => {
-    res.render('about', {title : 'About me'});
-})
-
-app.get('/projects', (req, res) => {
-    res.render('projects', {title : 'Projects'});
-})
-
-app.get('/services', (req, res) => {
-    res.render('services', {title : 'Services'});
-})
-
-app.get('/contact', (req, res) => {
-    res.render('contact', {title : 'Contact'});
-})
-
-app.all('*', (req, res) => {
-    res.status(404).send('Page Not Found')
-})
+app.use('/', homeRoute);
+app.use('/about', aboutRoute);
+app.use('/projects', projectsRoute);
+app.use('/services', servicesRoute);
+app.use('/contact', contactRoute);
 
 app.listen(3000,() => {
     console.log('server is listening on port 5000')
