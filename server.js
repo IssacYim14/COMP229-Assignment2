42501917e7b16d33c5e7a51277586971c20e288b
IssacYim14 <134939017+IssacYim14@users.noreply.github.com>
2023-06-03 10:28:28 -0400

    added ejs files

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 const express = require('express');
+//let ejs = require('ejs');
 const path = require('path');
 const app = express();
 
 // setup static and middleware
 app.use(express.static('./public'))
-app.set('views', './view');
+//app.set('views', './view');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './home.html'))
+    res.render('home', {title : 'Home Page'});
+    //res.sendFile(path.resolve(__dirname, './home.html'))
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './about.html'))
+    res.render('about', {title : 'About me'});
 })
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './projects.html'))
+    res.render('projects', {title : 'Projects'});
 })
 
 app.get('/services', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './services.html'))
+    res.render('services', {title : 'Services'});
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './contact.html'))
+    res.render('contact', {title : 'Contact'});
 })
 
 app.all('*', (req, res) => {
-    res.status(404).send('Not Found')
+    res.status(404).send('Page Not Found')
 })
 
 app.listen(3000,() => {
