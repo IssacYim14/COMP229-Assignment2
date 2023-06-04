89d4a131240c44c446a8731c767654052386fe3b
IssacYim14 <134939017+IssacYim14@users.noreply.github.com>
2023-06-04 13:21:44 -0400

    add comments

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,13 @@
+/*
+File Name: contact.js
+Created by: Chun Wai Yim (Student #: 301242959) on 6/2/2023
+*/
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    // dynamic change the title in ejs
     res.render('contact', {title : 'Contact'});
 })
 
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+/*
+File Name: server.js
+Created by: Chun Wai Yim (Student #: 301242959) on 6/2/2023
+*/
+
 const express = require('express');
 const app = express();
 
 app.use(express.static('./public'))
-//app.set('views', './view');
 app.set('view engine', 'ejs');
 
+// require routes of different pages
 const homeRoute = require('./routes/home');
 const aboutRoute = require('./routes/about');
 const projectsRoute = require('./routes/projects');
 const servicesRoute = require('./routes/services');
 const contactRoute = require('./routes/contact');
 
+// express use routes
 app.use('/', homeRoute);
 app.use('/about', aboutRoute);
 app.use('/projects', projectsRoute);
