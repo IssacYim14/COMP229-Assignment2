dfedbede9f8bc6d56376c181659a9ea35e51e5ce
IssacYim14 <134939017+IssacYim14@users.noreply.github.com>
2023-06-17 22:37:08 -0400

    add login Page

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+router.get('/login', (req, res) => res.render('login'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,19 +9,13 @@ const app = express();
 app.use(express.static('./public'))
 app.set('view engine', 'ejs');
 
-// require routes of different pages
-const homeRoute = require('./routes/home');
-const aboutRoute = require('./routes/about');
-const projectsRoute = require('./routes/projects');
-const servicesRoute = require('./routes/services');
-const contactRoute = require('./routes/contact');
-
 // express use routes
-app.use('/', homeRoute);
-app.use('/about', aboutRoute);
-app.use('/projects', projectsRoute);
-app.use('/services', servicesRoute);
-app.use('/contact', contactRoute);
+app.use('/', require('./routes/home'));
+app.use('/about', require('./routes/about'));
+app.use('/projects', require('./routes/projects'));
+app.use('/services', require('./routes/services'));
+app.use('/contact', require('./routes/contact'));
+app.use('/users', require('./routes/users'));
 
 app.listen(process.env.PORT || 3000,() => {
     console.log('server is listening on port 3000');
